f61dc56fa1895dd796cb1700ded1035a4f957d4b
ehabay18 <adityabayupratama1811@gmail.com>
2024-07-28 16:33:43 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,76 @@
+let jsValue = `  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/vars.css">
+    <link rel="stylesheet" href="./css/style.css">
+    
+    
+    <style>
+    a,
+    button,
+    input,
+    select,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        border: none;
+        text-decoration: none;
+        background: none;
+    
+        -webkit-font-smoothing: antialiased;
+    }
+    
+    menu, ol, ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+    </style>
+    <title>LET IT FLOW</title>
+  </head>
+  <body>
+    <div class="page-1">
+      <div class="frame">
+        <img class="group" src="./image/Background.svg" />
+      </div>
+      <div class="let-it-flow">LET IT FLOW</div>
+      <div class="to">TO</div>
+      <div class="welcome">WELCOME</div>
+      <a href="page3.html"<a/><div class="menu">Menu</div>
+      <a href="Page2.html"</a/> <div class="lokasi">Lokasi</div> 
+      <a href="index.html"></a><div class="home">Home</div>
+      <div class="_2024-copyright-let-it-flow">2024 COPYRIGHT LET IT FLOW</div>
+      <div class="c">C</div>
+      <div class="tekstur-bentu-dan-ciri-khas-yang-melembangkan-Coffeeshop-Let-It-Flow">
+        tekstur, bentuk, dan ciri khas yang melembangkan Coffeeshop Let It Flow.
+      </div>
+      <div class="standar-hidangan-makanan-dan-minuman-yang-disediakan-memiliki-rasa">
+        standar. Hidangan makanan dan minuman yang disediakan memiliki rasa,
+      </div>
+      <div class="yang-berkualitas-dengan-resep-dan-teknik-serta-prosedure-yang-sesuai">
+        yang berkualitas dengan resep dan teknik serta prosedure yang sesuai
+      </div>
+      <div class="Makanan-dan-minuman-yang-disajikan-tentunya-menggunakan-bahan-bahan">
+        Makanan dan minuman yang disajikan tentunya menggunakan bahan-bahan
+      </div>
+      <div class="yang-dimana-usaha-ini-memfokuskan-pada-makanan-dan-minuman">
+        Yang Dimana usaha ini memfokuskan pada hidangan makanan dan
+        minuman
+      </div>
+      <div class="let-it-flow-ini-merupakan-cofeeshop-yang-berada-dibandung">
+        Let It Flow ini merupakan Cofeeshop yang berada dikota Bandung.
+      </div>
+      <div class="apa-itu-let-if-flow">Apa itu Let if Flow?</div>
+    </div>
+    
+  </body>
+  </html>`;
---
+++ js/page3.js
@@ -0,0 +1,55 @@
+let jsValue = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/vars.css">
+  <link rel="stylesheet" href="./css/Page3.css">
+  
+  
+  <style>
+   a,
+   button,
+   input,
+   select,
+   h1,
+   h2,
+   h3,
+   h4,
+   h5,
+   * {
+       box-sizing: border-box;
+       margin: 0;
+       padding: 0;
+       border: none;
+       text-decoration: none;
+       background: none;
+   
+       -webkit-font-smoothing: antialiased;
+   }
+   
+   menu, ol, ul {
+       list-style-type: none;
+       margin: 0;
+       padding: 0;
+   }
+   </style>
+  <title>Menu</title>
+</head>
+<body>
+  <div class="page-2">
+    <div class="frame">
+      <img class="group" src="./image/Frame.svg" />
+    </div>
+    <a href="page3.html"</a><div class="menu">Menu</div>
+      <a href="Page2.html"</a> <div class="lokasi">Lokasi</div> 
+      <a href="index.html"</a><div class="home">Home</div>
+    <div class="_2024-copyright-let-it-flow">2024 COPYRIGHT LET IT FLOW</div>
+    <div class="c">C</div>
+    <div class="our">OUR</div>
+    <div class="menu2">MENU</div>
+    <div class="LET-IT-FLOW">LET IT FLOW</div>
+    
+</body>
+</html>`;
